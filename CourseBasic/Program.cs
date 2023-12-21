60e24e6bc8766caa972bf2a9867bc13fa089643f
Andressa Dal Col <andressadomingosdalcol@gmail.com>
2023-12-21 06:25:17 -0300

    Adicionar arquivos de projeto.

---
+++ CourseBasic/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CourseBasic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string nome; 
+            double quarto;
+            int preco;
+
+
+            Console.WriteLine("Qual seu nome completo:");
+            nome = Console.ReadLine();
+            Console.WriteLine(nome);
+
+            Console.WriteLine("Quantos quartos tem na sua casa:");
+            quarto = double.Parse(Console.ReadLine());
+            Console.WriteLine(quarto);
+            
+
+
+            Console.WriteLine("Entre com o preço de um produto:");
+            preco = int.Parse(Console.ReadLine());
+            Console.WriteLine(preco);
+            Console.ReadLine();
+            
+            
+            
+            
+            
+            
+            
+            
+            
+        
+
+
+
+
+        }
+    }
+}
